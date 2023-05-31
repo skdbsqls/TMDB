63de5ecaa6bcd1616335348bb22d31aa48d1b788
skdbsqls <skdbsqls98@naver.com>
2023-05-31 16:47:18 +0900

    카드 리스트 UI 구현 완료 (alert창까지)

--- movie.js
+++ movie.js
@@ -1,5 +1,6 @@
-const section = document.querySelector(".cardlist");
-const imgUrl = "https://image.tmdb.org/t/p/w300";
+const baseurl = "https://image.tmdb.org/t/p/w300";
+const $section = document.querySelector(".cardlist");
+
 const options = {
   method: "GET",
   headers: {
@@ -15,25 +16,44 @@ fetch(
 )
   .then((response) => response.json())
   .then(function (response) {
+    // 여러 개의 영화정보가 담긴 results[배열]를 movies라는 변수에 담기
     const movies = response.results;
 
-    movies.map(function (moviecard) {
+    // movies[배열]를 돌면서 movie[배열요소](영화 한 개당 정보) 받아오기
+    movies.map(function (movie) {
+      // 영화정보를 담을 태그들 만들어주기 (div: 카드/img: 포스터/title: 제목/overview: 요약/average: 평점)
       const div = document.createElement("div");
       const img = document.createElement("img");
       const title = document.createElement("h3");
       const overview = document.createElement("p");
       const average = document.createElement("p");
 
-      img.setAttribute("src", imgUrl + moviecard.poster_path);
-      title.innerHTML = `${moviecard.title}`;
-      overview.innerHTML = `${moviecard.overview}`;
-      average.innerHTML = `${moviecard.vote_average}`;
+      // div태그의 class 값 지정하기
+      div.setAttribute("class", "moviecard");
+      // div태그의 id 값을 영화정보의 id를 받아와서 넣어주기
+      div.setAttribute("id", movie.id);
+      // onclick
+      div.setAttribute("onclick", "alertOn(this)");
 
+      // img태그의 src(이미지소스 url) 속성값을 baseurl+영화정보의 poster_path를 받아와서 넣어주기
+      img.setAttribute("src", baseurl + movie.poster_path);
+      // title,overview,average 태그에 영화정보의 title,overview,average를 받아와서 넣어주기
+      title.innerHTML = `${movie.title}`;
+      overview.innerHTML = `${movie.overview}`;
+      average.innerHTML = `${movie.vote_average}`;
+
+      // img, title, overview, average 태그를 div태그 밑으로 넣어주기
       div.appendChild(img);
       div.appendChild(title);
       div.appendChild(overview);
       div.appendChild(average);
-      section.appendChild(div);
+      // div태그를 section태그 밑으로 넣어주기
+      $section.appendChild(div);
     });
   })
   .catch((err) => console.error(err));
+
+function alertOn(moviecard) {
+  const movieID = moviecard.getAttribute("id");
+  alert(`영화 id : ${movieID}`);
+}
